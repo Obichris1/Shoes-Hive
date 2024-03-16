a0e31822a9c4848fc0fe65aecd0889ba0e58d09f
Obichris1 <obichris202@gmail.com>
2024-03-16 23:34:40 +0100

    commit

--- app/redux/shoppingSlice.js
+++ app/redux/shoppingSlice.js
@@ -1,23 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    productData : [],
-    userInfo : null ,
-    orderData : []
-}
-
+  productData: [],
+  userInfo: null,
+  orderData: [],
+};
 
 export const shoppingSlice = createSlice({
-    name : 'shopping',
-    initialState,
-    reducers : {
-        addToCart : (state, action) => {
-            state.productData = action.payload
-        }
-    }
-})
-
+  name: "shopping",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+    //   state.productData = action.payload
+      const existingProduct = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += action.payload.quantity;
+        console.log(state.productData);
+      } else {
+        state.productData.push(action.payload);
+      }
+    },
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const {addToCart } = shoppingSlice.actions
+export const { addToCart } = shoppingSlice.actions;
 
-export default shoppingSlice.reducer
+export default shoppingSlice.reducer;
